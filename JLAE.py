0a2ef50cb512fe8c072519541f1ad72cb1f3898b
ziyang997 <56155063+ziyang997@users.noreply.github.com>
2020-12-24 16:38:09 +0800

    Update JLAE.py

--- JLAE.py
+++ JLAE.py
@@ -37,11 +37,12 @@ class JoVA():
         self.user_train = data.user_train
         self.train_R = data.train_R
         self.test_R = data.test_R
+        self.coffi_u= data.coffi
+        self.coffi_i =data.coffi.T
 
         self.train_epoch = args.train_epoch
         self.batch_size = args.batch_size
 
-        # self.train_num = self.train_R.sum()
         self.num_batch_u = int(math.ceil(self.num_users / float(self.batch_size)))
         self.num_batch_i = int(math.ceil(self.num_items / float(self.batch_size)))
 
@@ -49,106 +50,71 @@ class JoVA():
 
         self.users = tf.placeholder(tf.int32, shape=(None,))
         self.items = tf.placeholder(tf.int32, shape=(None,))
+        self.drop = tf.placeholder(tf.float32)
         self.input_R_U = tf.placeholder(dtype=tf.float32, shape=[None, self.num_items], name="input_R_U")
         self.input_R_I = tf.placeholder(dtype=tf.float32, shape=[self.num_users, None], name="input_R_I")
         self.input_P_cor = tf.placeholder(dtype=tf.int32, shape=[None, 2], name="input_P_cor")
         self.input_N_cor = tf.placeholder(dtype=tf.int32, shape=[None, 2], name="input_N_cor")
-        self.user_pos_item = tf.nn.embedding_lookup(self.item_train, self.users)
-        self.item_pos_user = tf.nn.embedding_lookup(self.user_train, self.items)
-
+        self.train_coffi_u = tf.nn.embedding_lookup(self.coffi_u, self.users)
+        self.train_coffi_i = tf.nn.embedding_lookup(self.coffi_i, self.items)
         self.u_embeddings_c = self.user_e()
         self.i_embeddings = self.user_d()
 
         self.i_embeddings_c = self.item_e()
         self.u_embeddings = self.item_d()
 
-        # self.u_g_embeddings = tf.nn.embedding_lookup(self.u_embeddings, self.users)
-        # self.rating_i = self._pre_i()
-        # self.rating_i = tf.reshape(self.rating_u, [-1, self.num_items + 1])
-
-        self.loss = self.get_loss()
-        # self.rating = tf.reshape(self.rating, [-1, self.num_items + 1])
 
         self.optimizer = tf.train.AdamOptimizer(learning_rate=self.lr).minimize(self.loss)
 
     def user_e(self):
         # encoder
         self.Wue = tf.get_variable('Wue', [self.num_items, latent_dim], tf.float32, xavier_initializer())
-        # self.h = tf.get_variable('h', [1, latent_dim], tf.float32, xavier_initializer())
-        u_embedding_c = tf.matmul(tf.square(tf.nn.l2_normalize(self.input_R_U, 1)), self.Wue)
+        self.nor_input_u = tf.square(tf.nn.l2_normalize(self.input_R_U, 1))
+        self.nor_input_u  = tf.nn.dropout(self.nor_input_u, self.drop)
+        u_embedding_c = tf.matmul(self.nor_input_u, self.Wue)
 
         return u_embedding_c
 
     def user_d(self):
         # encoder
-        self.Wud = tf.get_variable('Wud', [self.num_items + 1, latent_dim], tf.float32, xavier_initializer())
+        self.Wud = tf.get_variable('Wud', [self.num_items, latent_dim], tf.float32, xavier_initializer())
         i_embedding = self.Wud
 
         return i_embedding
 
     def item_e(self):
         # encoder
-        input_R_I_T = tf.transpose(self.input_R_I)
 
+        self.nor_input_i = tf.square(tf.nn.l2_normalize(tf.transpose(self.input_R_I), 1))
+        self.nor_input_i= tf.nn.dropout(self.nor_input_i, self.drop)
         self.Wie = tf.get_variable('Wie', [self.num_users, latent_dim], tf.float32, xavier_initializer())
         # self.h = tf.get_variable('h', [1, latent_dim], tf.float32, xavier_initializer())
-        i_embedding_c = tf.matmul(tf.square(tf.nn.l2_normalize(input_R_I_T, 1)), self.Wie)
+        i_embedding_c = tf.matmul(self.nor_input_i, self.Wie)
 
         return i_embedding_c
 
     def item_d(self):
         # encoder
-        self.Wid = tf.get_variable('Wid', [self.num_users + 1, latent_dim], tf.float32, xavier_initializer())
+        self.Wid = tf.get_variable('Wid', [self.num_users , latent_dim], tf.float32, xavier_initializer())
         u_embedding = self.Wid
 
         return u_embedding
 
     def get_loss(self):
-        pos_item = tf.nn.embedding_lookup(self.i_embeddings, self.user_pos_item)
-        pos_num_r = tf.cast(tf.not_equal(self.user_pos_item, self.num_items), 'float32')
-        pos_item = tf.einsum('ab,abc->abc', pos_num_r, pos_item)
-        pos_r = tf.einsum('ac,abc->ab', self.u_embeddings_c, pos_item)
-        pos_r = tf.reshape(pos_r, [-1, self.max_item])
-        loss_u = 0.5 * tf.reduce_sum(
-            tf.reduce_sum(tf.reduce_sum(tf.einsum('ab,ac->abc', self.i_embeddings, self.i_embeddings), 0)
-                          * tf.reduce_sum(tf.einsum('ab,ac->abc', self.u_embeddings_c, self.u_embeddings_c), 0)
-                          , 0), 0)
-        loss_u += tf.reduce_sum((1.0 - 0.5) * tf.square(pos_r) - 2.0 * pos_r)
-        u_batch = tf.nn.embedding_lookup(self.u_embeddings, self.users)
-        i_batch = tf.nn.embedding_lookup(self.i_embeddings, self.items)
-        self.rating_u = tf.matmul(self.u_embeddings_c, tf.transpose(i_batch))
-        self.rating_i = tf.matmul(self.i_embeddings_c, tf.transpose(u_batch))
-        self.rating = (self.rating_u + tf.transpose(self.rating_i)) / 2
-        pos_data = tf.gather_nd(self.rating, self.input_P_cor)
-        neg_data = tf.gather_nd(self.rating, self.input_N_cor)
-
+        self.u_decoder = tf.matmul(self.u_embeddings_c, self.i_embeddings, transpose_a=False, transpose_b=True)
+        self.i_decoder = tf.matmul(self.i_embeddings_c, self.u_embeddings, transpose_a=False, transpose_b=True)
+        self.decoder = ((tf.transpose(tf.gather_nd(tf.transpose(self.u_decoder), self.items)))
+                        + tf.gather_nd(tf.transpose(self.i_decoder), self.users)) / 2.0
+        pos_data = tf.gather_nd(self.Decoder, self.input_P_cor)
+        neg_data = tf.gather_nd(self.Decoder, self.input_N_cor)
         pre_cost1 = tf.maximum(neg_data - pos_data + 0.15,
                                tf.zeros(tf.shape(neg_data)[0]))
-        hinge_loss = tf.reduce_sum(pre_cost1)  # prediction squared error
+        self.hinge = tf.reduce_sum(pre_cost1)
+        self.loss_u = tf.reduce_sum(tf.square(self.u_decoder-self.input_R_U)*self.train_coffi_u)
+        self.loss_i = tf.reduce_sum(tf.square(self.i_decoder-tf.transpose(self.input_R_I))*self.train_coffi_i)
 
-        pos_user = tf.nn.embedding_lookup(self.u_embeddings, self.item_pos_user)
-        pos_num_r = tf.cast(tf.not_equal(self.item_pos_user, self.num_users), 'float32')
+        self.loss = (self.loss_u  + self.loss_i)/2.0 + self.hinge
 
-        pos_user = tf.einsum('ab,abc->abc', pos_num_r, pos_user)
-        pos_r = tf.einsum('ac,abc->ab', self.i_embeddings_c, pos_user)
-        pos_r = tf.reshape(pos_r, [-1, self.max_user])
-        loss_i = 0.5 * tf.reduce_sum(
-            tf.reduce_sum(tf.reduce_sum(tf.einsum('ab,ac->abc', self.u_embeddings, self.u_embeddings), 0)
-                          * tf.reduce_sum(tf.einsum('ab,ac->abc', self.i_embeddings_c, self.i_embeddings_c), 0)
-                          , 0), 0)
-        loss_i += tf.reduce_sum((1.0 - 0.5) * tf.square(pos_r) - 2.0 * pos_r)
-
-        return 0.5 * (loss_u + loss_i) + hinge_loss + 1e-3 * (
-                    tf.nn.l2_loss(self.Wie) + tf.nn.l2_loss(self.Wid) + tf.nn.l2_loss(self.Wue) + tf.nn.l2_loss(
-                self.Wud))
-
-    def _pre_u(self):
-        pre = tf.matmul(self.u_embeddings_c, self.i_embeddings, transpose_a=False, transpose_b=True)
-        return pre
-
-    def _pre_i(self):
-        pre = tf.matmul(self.i_embeddings_c, self.u_embeddings, transpose_a=False, transpose_b=True)
-        return pre
 
 
 if __name__ == '__main__':
@@ -185,7 +151,7 @@ if __name__ == '__main__':
                     [model.optimizer, model.loss],
                     feed_dict={model.users: row_idx, model.input_R_U: input_R_U, model.items: col_idx,
                                model.input_R_I: input_R_I, model.input_P_cor: p_input,
-                               model.input_N_cor: n_input})
+                               model.input_N_cor: n_input, model.drop: 0.3})
                 loss += batch_loss / model.num_batch_i * model.num_batch_u
 
         print("Epoch %d //" % (epoch), " cost = {:.8f}".format(loss), "Elapsed time : %d sec" % (time.time() - t1))
